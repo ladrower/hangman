160bc44d5e6dee345fd37ff1e49ff45657527362
Artem <ladrower@gmail.com>
2020-01-11 17:07:26 +0100

    refactor: game resource

--- src/app/Play/PlayStore.ts
+++ src/app/Play/PlayStore.ts
@@ -1,6 +1,6 @@
-import { action, computed, observable, when } from 'mobx'
+import { action, computed, observable } from 'mobx'
 import { store } from '@/infrastructure/store/store'
-import { HttpReader, HttpRequest, IResource } from '@/infrastructure/resource'
+import { HttpReader, IResource } from '@/infrastructure/resource'
 import { GameData } from '@/mock/game/GameServiceInterface'
 import { loadGame } from '@/app/Play/requests/loadGame'
 import { guess } from '@/app/Play/requests/guess'
@@ -11,10 +11,22 @@ export class PlayStore {
     private httpReader: HttpReader,
   ) {}
 
-  private disposeGameResource: () => void = () => null
+  private gameData: GameData | null = null
+
   @observable private gameResource: IResource<GameData> = {}
-  @observable game: GameData | null = null
-  @observable error? = ''
+
+  @computed get game(): GameData |null {
+    const { data } = this.gameResource
+    if (data) {
+      this.gameData = data
+    }
+    return this.gameData
+  }
+
+  @computed get error(): string {
+    const { error } = this.gameResource
+    return error && error.response.data
+  }
 
   @computed get charsUsed() {
     const { game } = this
@@ -25,24 +37,11 @@ export class PlayStore {
     return this.gameResource.loading
   }
 
-  @action makeGuess =(character: string) =>
-    this.updateGameResource(guess(character))
+  @action makeGuess (character: string) {
+    this.gameResource = this.httpReader.read(guess(character))
+  }
 
-  @action loadGame = (override = false) =>
-    this.updateGameResource(loadGame(override))
-
-  private updateGameResource(request: HttpRequest<GameData>) {
-    this.disposeGameResource()
-    this.error = ''
-    this.gameResource = this.httpReader.read(request)
-    this.disposeGameResource = when(() => !this.gameResource.loading, () => {
-      const { data, error } = this.gameResource
-      if (data) {
-        this.game = data
-      } else if (error) {
-        this.error = error.response.data
-      }
-    })
-    return this.disposeGameResource
+  @action loadGame (override = false) {
+    this.gameResource = this.httpReader.read(loadGame(override))
   }
 }
