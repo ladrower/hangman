b03c9f5c559236c2571ea0d0fc5ea3e0808ebbaf
Artem <ladrower@gmail.com>
2020-01-14 13:03:43 +0100

    test: snapshot basic components

--- src/app/components/__tests__/LoadingIndicator.spec.tsx
+++ src/app/components/__tests__/LoadingIndicator.spec.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { LoadingIndicator } from '../LoadingIndicator'
 
+const Spinner = () => <div>spinner...</div>
+
 it('should render spinner', () => {
-    const Spinner = () => <div>spinner...</div>
     const tree = renderer
         .create(<LoadingIndicator resource={{ loading: true }} component={Spinner}/>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it('should not render spinner', () => {
+    const tree = renderer
+        .create(<LoadingIndicator resource={{ loading: false }} component={Spinner}/>)
         .toJSON();
     expect(tree).toMatchSnapshot();
 });
